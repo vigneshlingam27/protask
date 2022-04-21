d9e0a01ccedd011b794aed7a95226c373a6b4363
vignehlingam27 <vigneshlingam1@gmail.com>
2022-04-21 11:30:21 +0530

    cnu code

--- src/org/test2/ProjectTask21.java
+++ src/org/test2/ProjectTask21.java
@@ -3,7 +3,7 @@ package org.test2;
 public class ProjectTask21 {
 	public static void main(String[] args) {
 		System.out.println("sample test");
-		System.out.println("not merged");
+		System.out.println("cnu not  merged");
 	}
 
 }
